c3a6ffb8e7028a0cc02788f85c88008fbab75d8d
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2023-04-28 01:36:17 +0530

    remove branding

--- app/javascript/shared/components/Branding.vue
+++ app/javascript/shared/components/Branding.vue
@@ -2,23 +2,7 @@
   <div
     v-if="globalConfig.brandName && !disableBranding"
     class="px-0 py-3 flex justify-center"
-  >
-    <a
-      :href="brandRedirectURL"
-      rel="noreferrer noopener nofollow"
-      target="_blank"
-      class="branding--link justify-center"
-    >
-      <img
-        class="branding--image"
-        :alt="globalConfig.brandName"
-        :src="globalConfig.logoThumbnail"
-      />
-      <span>
-        {{ useInstallationName($t('POWERED_BY'), globalConfig.brandName) }}
-      </span>
-    </a>
-  </div>
+  />
   <div v-else class="p-3" />
 </template>
 
